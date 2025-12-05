676224862613a5b6f2d19a16ea090f860291e3e4
sdhutchins <sdhutchins@uab.edu>
2025-12-04 18:19:16 -0600

    Add simple app.py with nav bar.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
-MyApp = Flask(__name__)
 
-@MyApp.route("/")
-def hello():
-	return render_template('index.html')
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template("index.html")
 
 if __name__ == "__main__":
-	MyApp.run()
+	app.run()
