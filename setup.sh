5abc39caa7a9e04051aee425b94be1a18494d036
sdhutchins <sdhutchins@uab.edu>
2025-12-04 18:52:58 -0600

    Symlink in setup.sh.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,11 @@ set -eo pipefail
 IFS=$'\n\t'
 
 module load Python
-python -m virtualenv .venv
-source .venv/bin/activate
+python -m virtualenv venv
+source venv/bin/activate
 pip install -r requirements.txt
+
+# Create bin directory if it doesn't exist
+mkdir -p bin
+# Create symlink to python in venv
+ln -sf ../venv/bin/python bin/python
